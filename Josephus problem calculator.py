7beab8d8495af28b46c5147157cc7c7b3f2e44f2
JTLongwe <66729322+JTLongwe@users.noreply.github.com>
2023-01-16 19:35:37 -0600

    Add files via upload
    This file contains two functions that calculate the Josephus number

---
+++ Josephus problem calculator.py
@@ -0,0 +1,13 @@
+def findTheWinner(n, k):
+    n = list(range(1,n+1))
+    print(josephus(n, k))
+        
+def josephus(ls, skip):
+    skip -= 1 
+    idx = skip
+    while len(ls) > 1:
+        ls.pop(idx)
+        idx = (idx + skip) % len(ls)
+    return ls[0]
+
+findTheWinner(10, 3)
